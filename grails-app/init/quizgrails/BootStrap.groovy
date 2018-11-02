d740ab823d46bc105235201349e61238b261694e
Shantall Giron <shantgiron@gmail.com>
2018-11-02 19:16:40 -0400

    Rol user added

--- grails-app/init/quizgrails/BootStrap.groovy
+++ grails-app/init/quizgrails/BootStrap.groovy
@@ -2,8 +2,25 @@ package quizgrails
 
 class BootStrap {
 
+    def springSecurityService
+
     def init = { servletContext ->
+        new ProductAnnouncement(message: 'Test').save()
+
+        def userRole = new Role('ROLE_USER').save()
+
+        def me = new User('shanty@hotmail.com', 'user').save()
+
+        UserRole.create me, userRole
+
+        UserRole.withSession {
+            it.flush()
+            it.clear()
+        }
+
     }
+
     def destroy = {
     }
+
 }
