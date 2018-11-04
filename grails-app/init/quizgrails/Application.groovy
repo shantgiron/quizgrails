7df4a1cab8281f1747aa229e7e1804a880ac6d03
Shantall Giron <shantgiron@gmail.com>
2018-11-03 20:07:07 -0400

    Update Application.groovy

--- grails-app/init/quizgrails/Application.groovy
+++ grails-app/init/quizgrails/Application.groovy
@@ -2,9 +2,12 @@ package quizgrails
 
 import grails.boot.GrailsApp
 import grails.boot.config.GrailsAutoConfiguration
+import org.springframework.context.annotation.Bean
+import org.springframework.validation.beanvalidation.MethodValidationPostProcessor
 
 class Application extends GrailsAutoConfiguration {
     static void main(String[] args) {
         GrailsApp.run(Application, args)
     }
+
 }
